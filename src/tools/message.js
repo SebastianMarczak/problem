b165f346369217a34a0f449fa4fca9a3f05a9b5d
Sebastian Marczak <sebastianmarczak87@gmail.com>
2023-01-21 16:12:19 +0100

    new

---
+++ src/tools/message.js
@@ -0,0 +1,7 @@
+export const message = (info) => console.log(info);
+export const messageDOM = (info) => {
+    const contener = document.querySelector("div");
+    contener.textContent = info;
+}
+const a = 43211113348744448713244;
+console.log(a);
